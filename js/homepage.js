70dd49fb994cb0a371acf3e079affe415f77a731
west <wxc422@163.com>
2015-08-18 19:25:19 +0800

    全屏CSS

--- js/homepage.js
+++ js/homepage.js
@@ -1,3 +1,17 @@
 /**
  * Created by CooTek on 2015/8/18.
  */
+
+/* ---------- jQuery组件：FullPageScrolling ---------- */
+(function($) {
+
+    var defaultOptions = {
+        'container': '#page-container',         // 滚动页面所在的父级
+        'sections': '.page-section',            // 单一页面 类名
+        'timingFunction': 'ease',               // 滚动所用的时间函数
+        'duration': 1000,                       // 滚动一屏所需要的时间
+    };
+
+
+
+})(jQuery);
